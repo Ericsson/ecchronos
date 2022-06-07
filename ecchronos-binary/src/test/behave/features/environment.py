446481a5bc451cbe1b206e47f4eb2d56ba538634
Maciej Sokol <maciej.sokol@ericsson.com>
2022-06-07 14:02:40 +0200

    Add missing license header and remove debug prints

--- ecchronos-binary/src/test/behave/features/environment.py
+++ ecchronos-binary/src/test/behave/features/environment.py
@@ -1,15 +1,17 @@
-#  Copyright 2022 Telefonaktiebolaget LM Ericsson
 #
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#      http://www.apache.org/licenses/LICENSE-2.0
+# Copyright 2022 Telefonaktiebolaget LM Ericsson
 #
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 import ssl
 from cassandra.cluster import Cluster  # pylint: disable=no-name-in-module
@@ -25,8 +27,6 @@ def before_all(context):
     auth_provider=None
     if (username and username != '') and (password and password != ''):
         auth_provider = PlainTextAuthProvider(username=username, password=password)
-    else:
-        print 'Username or password empty, will try to connect without'
 
     no_tls = context.config.userdata.get("no_tls")
     if no_tls:
