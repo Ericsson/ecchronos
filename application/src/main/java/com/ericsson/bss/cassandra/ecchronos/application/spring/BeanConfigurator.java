6adb5ae932a210d50f8e8579f33833d0b1f5693d
Maciej Sokol <maciej.sokol@ericsson.com>
2022-09-23 11:11:21 +0200

    Split metrics into separate connector
    For testing:
    curl http://localhost:8080/metrics -> 404
    curl http://localhost:8081/metrics -> 200
    curl http://localhost:8080/repair-management/v2/schedules -> 200
    curl http://localhost:8081/repair-management/v2/schedules -> 404

--- application/src/main/java/com/ericsson/bss/cassandra/ecchronos/application/spring/BeanConfigurator.java
+++ application/src/main/java/com/ericsson/bss/cassandra/ecchronos/application/spring/BeanConfigurator.java
@@ -14,6 +14,7 @@
  */
 package com.ericsson.bss.cassandra.ecchronos.application.spring;
 
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Arrays;
@@ -30,8 +31,10 @@ import com.ericsson.bss.cassandra.ecchronos.connection.CertificateHandler;
 import com.ericsson.bss.cassandra.ecchronos.core.repair.DefaultRepairConfigurationProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.convert.ApplicationConversionService;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -57,15 +60,30 @@ import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.dropwizard.DropwizardExports;
 import io.prometheus.client.exporter.MetricsServlet;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 public class BeanConfigurator
 {
+    @Value("${metricsServer.port:8081}")
+    private int metricsPort;
+
+    @Value("${metricsServer.enabled:false}")
+    private Boolean isMetricsServerEnabled;
+
     private static final Logger LOG = LoggerFactory.getLogger(BeanConfigurator.class);
 
     private static final String CONFIGURATION_FILE = "ecc.yml";
     private static final String SECURITY_FILE = "security.yml";
+    private static final String METRICS_ENDPOINT = "/metrics";
 
     private final AtomicReference<Security.CqlSecurity> cqlSecurity = new AtomicReference<>();
     private final AtomicReference<Security.JmxSecurity> jmxSecurity = new AtomicReference<>();
@@ -229,8 +247,46 @@ public class BeanConfigurator
 
         collectorRegistry.register(new DropwizardExports(metricRegistry, metricFilter));
         servletRegistrationBean.setServlet(new MetricsServlet(collectorRegistry));
-        servletRegistrationBean.setUrlMappings(Arrays.asList("/metrics/*"));
+        servletRegistrationBean.setUrlMappings(Arrays.asList(METRICS_ENDPOINT + "/*"));
         return servletRegistrationBean;
+    }
+
+    /**
+     * Creates a bean that filters requests based on URI and port.
+     *
+     * @return the filter that disallows /metric on any other port than metricsServer.port
+     * and disallows any endpoint besides /metric on metricsServer.port.
+     */
+    @Bean
+    public FilterRegistrationBean requestFilter()
+    {
+        Filter filter = new OncePerRequestFilter()
+        {
+            @Override
+            protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
+                    final FilterChain filterChain) throws ServletException, IOException
+            {
+                int requestPort = request.getLocalPort();
+                String requestURI = request.getRequestURI();
+                if (isMetricsServerEnabled && (requestURI.startsWith(METRICS_ENDPOINT) && requestPort != metricsPort))
+                {
+                    response.sendError(HttpStatus.NOT_FOUND.value());
+                }
+                else if (isMetricsServerEnabled && (!requestURI.startsWith(METRICS_ENDPOINT)
+                        && requestPort == metricsPort))
+                {
+                    response.sendError(HttpStatus.NOT_FOUND.value());
+                }
+                else
+                {
+                    filterChain.doFilter(request, response);
+                }
+            }
+        };
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(filter);
+        filterRegistrationBean.addUrlPatterns("/*");
+        return filterRegistrationBean;
     }
 
     private static StatementDecorator getStatementDecorator(final Config configuration) throws ConfigurationException
--- application/src/main/java/com/ericsson/bss/cassandra/ecchronos/application/spring/TomcatWebServerCustomizer.java
+++ application/src/main/java/com/ericsson/bss/cassandra/ecchronos/application/spring/TomcatWebServerCustomizer.java
@@ -14,48 +14,352 @@
  */
 package com.ericsson.bss.cassandra.ecchronos.application.spring;
 
-
+import org.apache.catalina.connector.Connector;
 import org.apache.coyote.http11.Http11NioProtocol;
+import org.apache.tomcat.util.net.SSLHostConfig;
+import org.apache.tomcat.util.net.jsse.PEMFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.Ssl;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.KeyStore;
+import java.security.cert.X509Certificate;
+
 @Component
 @EnableScheduling
-@SuppressWarnings({})
+@SuppressWarnings("PMD.TooManyFields")
 public class TomcatWebServerCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory>
 {
-    private Http11NioProtocol http11NioProtocol;
+    private static final Logger LOG = LoggerFactory.getLogger(TomcatWebServerCustomizer.class);
+
+    private Http11NioProtocol defaultServerHttp11NioProtocol;
+    private Http11NioProtocol metricsServerHttp11NioProtocol;
+    private SSLHostConfig defaultSSLHostConfig;
+    private SSLHostConfig metricsSSLHostConfig;
 
     @Value("${server.ssl.enabled:false}")
-    private Boolean sslIsEnabled;
+    private Boolean isSslEnabled;
+
+    @Value("${server.ssl.key-store:#{null}}")
+    private String serverKeyStoreFile;
+
+    @Value("${server.ssl.key-store-password:#{null}}")
+    private String serverKeyStorePassword;
+
+    @Value("${server.ssl.key-store-type:JKS}")
+    private String serverKeyStoreType;
+
+    @Value("${server.ssl.key-alias:cert}")
+    private String serverKeyAlias;
+
+    @Value("${server.ssl.key-password:#{null}}")
+    private String serverKeyPassword;
+
+    @Value("${server.ssl.trust-store:#{null}}")
+    private String serverTrustStoreFile;
+
+    @Value("${server.ssl.trust-store-password:#{null}}")
+    private String serverTrustStorePassword;
+
+    @Value("${server.ssl.trust-store-type:JKS}")
+    private String serverTrustStoreType;
+
+    @Value("${server.ssl.certificate:#{null}}")
+    private String serverCertificate;
+
+    @Value("${server.ssl.certificate-private-key:#{null}}")
+    private String serverCertificatePrivateKey;
+
+    @Value("${server.ssl.trust-certificate:#{null}}")
+    private String serverTrustCertificate;
+
+    @Value("${server.ssl.client-auth:none}")
+    private String serverClientAuth;
+
+    @Value("${server.ssl.enabled-protocols:TLSv1.2}")
+    private String serverEnabledProtocols;
+
+    @Value("${server.ssl.ciphers:#{null}}")
+    private String serverCiphers;
+
+    @Value("${metricsServer.enabled:false}")
+    private Boolean isMetricsServerEnabled;
+
+    @Value("${metricsServer.ssl.enabled:false}")
+    private Boolean isMetricsSslEnabled;
+
+    @Value("${metricsServer.ssl.key-store:#{null}}")
+    private String metricsServerKeyStoreFile;
+
+    @Value("${metricsServer.ssl.key-store-password:#{null}}")
+    private String metricsServerKeyStorePassword;
+
+    @Value("${metricsServer.ssl.key-store-type:JKS}")
+    private String metricsServerKeyStoreType;
+
+    @Value("${metricsServer.ssl.key-alias:cert}")
+    private String metricsServerKeyAlias;
+
+    @Value("${metricsServer.ssl.key-password:#{null}}")
+    private String metricsServerKeyPassword;
+
+    @Value("${metricsServer.ssl.trust-store:#{null}}")
+    private String metricsServerTrustStoreFile;
+
+    @Value("${metricsServer.ssl.trust-store-password:#{null}}")
+    private String metricsServerTrustStorePassword;
+
+    @Value("${metricsServer.ssl.trust-store-type:JKS}")
+    private String metricsServerTrustStoreType;
+
+    @Value("${metricsServer.ssl.certificate:#{null}}")
+    private String metricsServerCertificate;
+
+    @Value("${metricsServer.ssl.certificate-private-key:#{null}}")
+    private String metricsServerCertificatePrivateKey;
+
+    @Value("${metricsServer.ssl.trust-certificate:#{null}}")
+    private String metricsServerTrustCertificate;
+
+    @Value("${metricsServer.ssl.client-auth:none}")
+    private String metricsServerClientAuth;
+
+    @Value("${metricsServer.ssl.enabled-protocols:TLSv1.2}")
+    private String metricsServerEnabledProtocols;
+
+    @Value("${metricsServer.ssl.ciphers:#{null}}")
+    private String metricsServerCiphers;
+
+    @Value("${metricsServer.port:8081}")
+    private int metricsServerPort;
 
     @Override
     public final void customize(final TomcatServletWebServerFactory factory)
     {
-        if (sslIsEnabled)
+        //Disable ssl on factory level, ssl will be configured for each connector instead.
+        Ssl ssl = factory.getSsl();
+        if (ssl != null)
+        {
+            ssl.setEnabled(false);
+        }
+        if (isSslEnabled)
         {
             factory.addConnectorCustomizers(connector ->
             {
-                http11NioProtocol = (Http11NioProtocol) connector.getProtocolHandler();
+                defaultServerHttp11NioProtocol = (Http11NioProtocol) connector.getProtocolHandler();
+                defaultSSLHostConfig = getSslHostConfig(serverKeyStoreFile, serverKeyStorePassword, serverKeyStoreType,
+                        serverKeyAlias, serverKeyPassword, serverTrustStoreFile, serverTrustStorePassword,
+                        serverTrustStoreType, serverCertificate, serverCertificatePrivateKey, serverTrustCertificate,
+                        serverEnabledProtocols, serverCiphers, serverClientAuth);
+                defaultServerHttp11NioProtocol.addSslHostConfig(defaultSSLHostConfig);
+                defaultServerHttp11NioProtocol.setSSLEnabled(true);
             });
+        }
+        if (isMetricsServerEnabled)
+        {
+            factory.addAdditionalTomcatConnectors(metricsConnector());
+        }
+    }
+
+    private Connector metricsConnector()
+    {
+        Connector connector = new Connector();
+        connector.setPort(metricsServerPort);
+        connector.setSecure(isMetricsSslEnabled);
+        connector.setScheme(getHttpScheme(isMetricsSslEnabled));
+        metricsServerHttp11NioProtocol = (Http11NioProtocol) connector.getProtocolHandler();
+        if (isMetricsSslEnabled)
+        {
+            metricsSSLHostConfig = getSslHostConfig(metricsServerKeyStoreFile, metricsServerKeyStorePassword,
+                    metricsServerKeyStoreType, metricsServerKeyAlias, metricsServerKeyPassword,
+                    metricsServerTrustStoreFile, metricsServerTrustStorePassword, metricsServerTrustStoreType,
+                    metricsServerCertificate, metricsServerCertificatePrivateKey, metricsServerTrustCertificate,
+                    metricsServerEnabledProtocols, metricsServerCiphers, metricsServerClientAuth);
+            metricsServerHttp11NioProtocol.addSslHostConfig(metricsSSLHostConfig);
+            metricsServerHttp11NioProtocol.setSSLEnabled(true);
+        }
+        return connector;
+    }
+
+    private String getHttpScheme(final boolean secure)
+    {
+        return secure ? "https" : "http";
+    }
+
+    @SuppressWarnings({ "PMD.ExcessiveParameterList", "checkstyle:ParameterNumber" })
+    private SSLHostConfig getSslHostConfig(
+            final String keystoreFile, final String keyStorePassword, final String keyStoreType, final String keyAlias,
+            final String keyPassword, final String trustStoreFile, final String trustStorePassword,
+            final String trustStoreType, final String certificate, final String certificatePrivateKey,
+            final String trustCertificate, final String enabledProtocols, final String ciphers, final String clientAuth)
+    {
+        SSLHostConfig sslHostConfig = new SSLHostConfig();
+        setCertificates(sslHostConfig, keystoreFile, keyStorePassword, keyStoreType, keyAlias, keyPassword, certificate,
+                certificatePrivateKey);
+        setTrustedCertificates(sslHostConfig, trustStoreFile, trustStorePassword, trustStoreType, trustCertificate);
+        sslHostConfig.setProtocols(enabledProtocols);
+        if (ciphers != null)
+        {
+            sslHostConfig.setCiphers(ciphers);
+        }
+        setAndValidateClientAuth(sslHostConfig, trustStoreFile, trustCertificate, clientAuth);
+        return sslHostConfig;
+    }
+
+    private void setCertificates(final SSLHostConfig sslHostConfig, final String keystoreFile,
+            final String keyStorePassword, final String keyStoreType, final String keyAlias, final String keyPassword,
+            final String certificate, final String certificatePrivateKey)
+    {
+        if (certificate != null && certificatePrivateKey != null)
+        {
+            sslHostConfig.setCertificateFile(getFilePath(certificate));
+            sslHostConfig.setCertificateKeyFile(getFilePath(certificatePrivateKey));
+        }
+        else if (keystoreFile != null)
+        {
+            sslHostConfig.setCertificateKeystoreFile(getFilePath(keystoreFile));
+            sslHostConfig.setCertificateKeystorePassword(keyStorePassword);
+            sslHostConfig.setCertificateKeystoreType(keyStoreType);
+            sslHostConfig.setCertificateKeyAlias(keyAlias);
+            sslHostConfig.setCertificateKeyPassword(keyPassword);
+        }
+        else
+        {
+            throw new IllegalStateException("key-store or certificate must be provided if using ssl.enabled 'true'");
+        }
+    }
+
+    private void setTrustedCertificates(final SSLHostConfig sslHostConfig, final String trustStoreFile,
+            final String trustStorePassword, final String trustStoreType, final String trustCertificate)
+    {
+        if (trustCertificate != null)
+        {
+            sslHostConfig.setTrustStore(getTrustStore(getFilePath(trustCertificate)));
+        }
+        else if (trustStoreFile != null)
+        {
+            sslHostConfig.setTruststoreFile(getFilePath(trustStoreFile));
+            sslHostConfig.setTruststorePassword(trustStorePassword);
+            sslHostConfig.setTruststoreType(trustStoreType);
+        }
+    }
+
+    private void setAndValidateClientAuth(final SSLHostConfig sslHostConfig, final String trustStoreFile,
+            final String trustCertificate, final String clientAuth)
+    {
+        if ("need".equalsIgnoreCase(clientAuth))
+        {
+            if (trustCertificate == null && trustStoreFile == null)
+            {
+                throw new IllegalStateException(
+                        "truststore or trust certificate must be provided if using client-auth 'need'");
+            }
+            sslHostConfig.setCertificateVerification(String.valueOf(SSLHostConfig.CertificateVerification.REQUIRED));
+        }
+        else if ("want".equalsIgnoreCase(clientAuth))
+        {
+            sslHostConfig.setCertificateVerification(String.valueOf(SSLHostConfig.CertificateVerification.OPTIONAL));
+        }
+    }
+
+    private String getFilePath(final String file)
+    {
+        return new File(file).toURI().toString();
+    }
+
+    private KeyStore getTrustStore(final String certificateAuthorities)
+    {
+        KeyStore trustStore = null;
+        try
+        {
+            trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            trustStore.load(null);
+
+            PEMFile certificateBundle = new PEMFile(certificateAuthorities);
+            for (X509Certificate certificate : certificateBundle.getCertificates())
+            {
+                trustStore.setCertificateEntry(certificate.getSerialNumber().toString(), certificate);
+            }
+        }
+        catch (GeneralSecurityException | IOException exception)
+        {
+            LOG.warn("Unable to load certificate authorities", exception);
+        }
+        return trustStore;
+    }
+
+    /**
+     * Reload the {@code SSLHostConfig} for the default server if SSL is enabled.
+     * Doing so should update ssl settings and fetch certificates from Keystores/PEM files.
+     * It reloads them every 60 seconds by default
+     */
+    @Scheduled(initialDelayString = "${server.ssl.refresh-rate-in-ms:60000}",
+               fixedRateString = "${server.ssl.refresh-rate-in-ms:60000}")
+    public void reloadSslContext()
+    {
+        if (isSslEnabled && defaultServerHttp11NioProtocol != null && defaultSSLHostConfig != null)
+        {
+            if (serverTrustCertificate != null)
+            {
+                reloadDefaultTrustStore();
+            }
+            defaultServerHttp11NioProtocol.reloadSslHostConfigs();
         }
     }
 
     /**
-     * Reload the {@code SSLHostConfig} if SSL is enabled. Doing so should update ssl settings and fetch
-     * certificates from Keystores. It reloads them every 60 seconds by default
+     * Reload the {@code SSLHostConfig} for the metrics server if SSL is enabled.
+     * Doing so should update ssl settings and fetch certificates from Keystores/PEM files.
+     * It reloads them every 60 seconds by default
      */
-    @Scheduled (initialDelayString = "${server.ssl.refresh-rate-in-ms:60000}",
-            fixedRateString = "${server.ssl.refresh-rate-in-ms:60000}")
-    public void reloadSslContext()
+    @Scheduled(initialDelayString = "${metricsServer.ssl.refresh-rate-in-ms:60000}",
+               fixedRateString = "${metricsServer.ssl.refresh-rate-in-ms:60000}")
+    public void reloadMetricsServerSslContext()
     {
-        if (sslIsEnabled && http11NioProtocol != null)
+        if (isMetricsSslEnabled && metricsServerHttp11NioProtocol != null && metricsSSLHostConfig != null)
         {
-            http11NioProtocol.reloadSslHostConfigs();
+            if (metricsServerTrustCertificate != null)
+            {
+                reloadMetricsTrustStore();
+            }
+            metricsServerHttp11NioProtocol.reloadSslHostConfigs();
         }
+    }
+
+    /**
+     * Only for tests.
+     */
+    void setMetricsPortProperty()
+    {
+        if (metricsServerHttp11NioProtocol != null)
+        {
+            System.setProperty("metricsServer.local.port",
+                    String.valueOf(metricsServerHttp11NioProtocol.getLocalPort()));
+        }
+    }
+
+    /**
+     * Reloads default server trustStore.
+     */
+    void reloadDefaultTrustStore()
+    {
+        defaultSSLHostConfig.setTrustStore(getTrustStore(getFilePath(serverTrustCertificate)));
+    }
+
+    /**
+     * Reloads metrics server trustStore.
+     */
+    void reloadMetricsTrustStore()
+    {
+        metricsSSLHostConfig.setTrustStore(getTrustStore(getFilePath(metricsServerTrustCertificate)));
     }
 }
