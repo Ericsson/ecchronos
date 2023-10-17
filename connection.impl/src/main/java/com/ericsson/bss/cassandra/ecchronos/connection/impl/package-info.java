12eda98a80ca199f8cc1fbcffe97170dd321a704
Maciej Sokol <maciej.sokol@ericsson.com>
2023-10-17 13:07:04 +0200

    Fix package-info for application and connection

--- connection.impl/src/main/java/com/ericsson/bss/cassandra/ecchronos/connection/impl/package-info.java
+++ connection.impl/src/main/java/com/ericsson/bss/cassandra/ecchronos/connection/impl/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Telefonaktiebolaget LM Ericsson
+ * Copyright 2023 Telefonaktiebolaget LM Ericsson
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,6 @@
  */
 
 /**
- *
+ * Contains the default implementation of API for outgoing connections (CQL and JMX) towards Cassandra.
  */
 package com.ericsson.bss.cassandra.ecchronos.connection.impl;
