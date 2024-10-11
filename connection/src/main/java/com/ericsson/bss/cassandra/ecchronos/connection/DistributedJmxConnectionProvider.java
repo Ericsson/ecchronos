e04989971dae9dd93b39c85ce51afe02fa852083
Paul Chandler <paul.chandler@ericsson.com>
2024-10-11 10:28:03 +0100

    #699 Reload nodes

--- connection/src/main/java/com/ericsson/bss/cassandra/ecchronos/connection/DistributedJmxConnectionProvider.java
+++ connection/src/main/java/com/ericsson/bss/cassandra/ecchronos/connection/DistributedJmxConnectionProvider.java
@@ -36,5 +36,5 @@ public interface DistributedJmxConnectionProvider extends Closeable
 
     void close(UUID nodeID) throws IOException;
 
-    void add( Node node) throws IOException;
+    void add(Node node) throws IOException;
 }
