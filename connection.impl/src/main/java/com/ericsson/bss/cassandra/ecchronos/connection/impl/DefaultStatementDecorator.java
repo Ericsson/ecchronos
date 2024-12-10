1d0708c745e799f775231b09656bb3a6888ae17c
tommy stendahl <tommy.stendahl@ericsson.com>
2024-12-10 16:53:15 +0100

    cleanup and fix related dependecy versions

--- connection.impl/src/main/java/com/ericsson/bss/cassandra/ecchronos/connection/impl/DefaultStatementDecorator.java
+++ connection.impl/src/main/java/com/ericsson/bss/cassandra/ecchronos/connection/impl/DefaultStatementDecorator.java
@@ -16,9 +16,7 @@ package com.ericsson.bss.cassandra.ecchronos.connection.impl;
 
 import com.datastax.oss.driver.api.core.cql.Statement;
 import com.ericsson.bss.cassandra.ecchronos.connection.StatementDecorator;
-import org.osgi.service.component.annotations.Component;
 
-@Component(service = StatementDecorator.class)
 public class DefaultStatementDecorator implements StatementDecorator
 {
     @Override
