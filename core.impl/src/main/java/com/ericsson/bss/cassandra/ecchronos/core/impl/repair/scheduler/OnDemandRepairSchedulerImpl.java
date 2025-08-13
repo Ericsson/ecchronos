e97d2efabec30be2dc88ae28330ca2c5fac1e29c
Paul Chandler <paul.chandler@ericsson.com>
2025-08-13 09:51:11 +0100

    #1054 ensure repair is run on the correct node.

--- core.impl/src/main/java/com/ericsson/bss/cassandra/ecchronos/core/impl/repair/scheduler/OnDemandRepairSchedulerImpl.java
+++ core.impl/src/main/java/com/ericsson/bss/cassandra/ecchronos/core/impl/repair/scheduler/OnDemandRepairSchedulerImpl.java
@@ -177,6 +177,7 @@ public final class OnDemandRepairSchedulerImpl implements OnDemandRepairSchedule
      *            The type of the repair.
      * @return Repair job view list
      */
+    @Override
     public List<OnDemandRepairJobView> scheduleClusterWideJob(final UUID nodeId, final TableReference tableReference,
                                                               final RepairType repairType) throws EcChronosException
     {
