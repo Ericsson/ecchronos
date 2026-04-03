553e5461460cf1700a9cb6841393796ac44eb148
0rlych1kk4 <orlychikka@gmail.com>
2026-04-03 23:45:07 +0800

    test: avoid modifying global scheduler frequency (prevent cross-test impact)

--- ecchronos-binary/src/test/bin/ecc_config.py
+++ ecchronos-binary/src/test/bin/ecc_config.py
@@ -72,7 +72,7 @@ class EcchronosConfig:
         return data
 
     def _modify_scheduler_configuration(self, data):
-        data["scheduler"]["frequency"]["time"] = 1
+        # 
         return data
 
     def _modify_twcs_configuration(self, data):
