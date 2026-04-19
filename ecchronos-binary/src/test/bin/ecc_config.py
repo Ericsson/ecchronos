b38cdf80297189b6b609f0996e00073ecdfb8bf8
0rlych1kk4 <orlychikka@gmail.com>
2026-04-19 17:37:37 +0800

    chore: apply license headers

--- ecchronos-binary/src/test/bin/ecc_config.py
+++ ecchronos-binary/src/test/bin/ecc_config.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-# vi: syntax=python
 #
-# Copyright 2025 Telefonaktiebolaget LM Ericsson
+# Copyright 2024 Telefonaktiebolaget LM Ericsson
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-#
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
