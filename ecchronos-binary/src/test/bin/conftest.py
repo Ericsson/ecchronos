b7cfffb272fa8a3cd35fb234c56db6d425182db7
0rlych1kk4 <orlychikka@gmail.com>
2026-05-02 23:01:39 +0800

    test: pass schedule keyspaces explicitly to python integration config

--- ecchronos-binary/src/test/bin/conftest.py
+++ ecchronos-binary/src/test/bin/conftest.py
@@ -47,6 +47,51 @@ class TestFixture:
             self.cassandra_cluster.create_cluster()
 
             logger.info("Configuring ecChronos")
+            self.cassandra_cluster.schedule_keyspaces = [
+                {
+                    "name": "test",
+                    "tables": [
+                        {
+                            "name": "table1",
+                            "interval": {"time": 1, "unit": "days"},
+                            "initial_delay": {"time": 1, "unit": "hours"},
+                            "unwind_ratio": 0.1,
+                            "alarm": {
+                                "warn": {"time": 4, "unit": "days"},
+                                "error": {"time": 8, "unit": "days"},
+                            },
+                        }
+                    ],
+                },
+                {
+                    "name": "test2",
+                    "tables": [
+                        {"name": "table1", "repair_type": "incremental"},
+                        {"name": "table2", "repair_type": "parallel_vnode"},
+                    ],
+                },
+                {
+                    "name": "system_auth",
+                    "tables": [
+                        {"name": "network_permissions", "enabled": False},
+                        {"name": "resource_role_permissons_index", "enabled": False},
+                        {"name": "role_members", "enabled": False},
+                        {"name": "role_permissions", "enabled": False},
+                        {"name": "roles", "enabled": False},
+                    ],
+                },
+                {
+                    "name": "ecchronos",
+                    "tables": [
+                        {"name": "lock", "enabled": False},
+                        {"name": "lock_priority", "enabled": False},
+                        {"name": "on_demand_repair_status", "enabled": False},
+                        {"name": "reject_configuration", "enabled": False},
+                        {"name": "repair_history", "enabled": False},
+                    ],
+                },
+            ]
+
             ecc_config = EcchronosConfig(context=self.cassandra_cluster)
             ecc_config.modify_configuration()
 
