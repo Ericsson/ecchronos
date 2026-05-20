300606c3152d25f36c816f371d31669ae99b2708
tommy stendahl <tommy.stendahl@ericsson.com>
2026-05-20 11:31:57 +0200

    Refactor DefaultRepairConfigurationProvider

---
+++ core.impl/src/main/java/com/ericsson/bss/cassandra/ecchronos/core/impl/repair/NodeLifecycleHandler.java
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2024 Telefonaktiebolaget LM Ericsson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ericsson.bss.cassandra.ecchronos.core.impl.repair;
+
+import com.datastax.oss.driver.api.core.metadata.Node;
+import com.ericsson.bss.cassandra.ecchronos.connection.DistributedJmxConnectionProvider;
+import com.ericsson.bss.cassandra.ecchronos.connection.DistributedNativeConnectionProvider;
+import com.ericsson.bss.cassandra.ecchronos.core.impl.multithreads.NodeWorkerManager;
+import com.ericsson.bss.cassandra.ecchronos.core.impl.refresh.NodeAddedAction;
+import com.ericsson.bss.cassandra.ecchronos.core.impl.refresh.NodeRemovedAction;
+import com.ericsson.bss.cassandra.ecchronos.core.repair.scheduler.ScheduleManager;
+import com.ericsson.bss.cassandra.ecchronos.data.sync.EccNodesSync;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ExecutorService;
+
+/**
+ * Handles node lifecycle events (up, down, add, remove) by coordinating
+ * JMX connections, node sync, worker management, and schedule creation.
+ */
+public class NodeLifecycleHandler
+{
+    private static final Logger LOG = LoggerFactory.getLogger(NodeLifecycleHandler.class);
+
+    private final EccNodesSync myEccNodesSync;
+    private final DistributedJmxConnectionProvider myJmxConnectionProvider;
+    private final DistributedNativeConnectionProvider myAgentNativeConnectionProvider;
+    private final NodeWorkerManager myWorkerManager;
+    private final ScheduleManager myScheduleManager;
+    private final ExecutorService myService;
+
+    public NodeLifecycleHandler(final EccNodesSync eccNodesSync,
+                                final DistributedJmxConnectionProvider jmxConnectionProvider,
+                                final DistributedNativeConnectionProvider agentNativeConnectionProvider,
+                                final NodeWorkerManager workerManager,
+                                final ScheduleManager scheduleManager,
+                                final ExecutorService service)
+    {
+        myEccNodesSync = eccNodesSync;
+        myJmxConnectionProvider = jmxConnectionProvider;
+        myAgentNativeConnectionProvider = agentNativeConnectionProvider;
+        myWorkerManager = workerManager;
+        myScheduleManager = scheduleManager;
+        myService = service;
+    }
+
+    /**
+     * Handle a node switching state to UP.
+     */
+    public void onUp(final Node node)
+    {
+        LOG.debug("{} switched state to UP.", node);
+        if (myAgentNativeConnectionProvider == null
+                || !myAgentNativeConnectionProvider.getNodes().containsKey(node.getHostId()))
+        {
+            onAdd(node);
+        }
+        else
+        {
+            LOG.info("Node {} came back up, refreshing JMX connection in case endpoint changed", node.getHostId());
+            NodeAddedAction callable = new NodeAddedAction(myEccNodesSync, myJmxConnectionProvider,
+                    myAgentNativeConnectionProvider, node);
+            myService.submit(callable);
+            if (myScheduleManager != null)
+            {
+                myScheduleManager.createScheduleFutureForNode(node.getHostId());
+            }
+            else
+            {
+                LOG.debug("myScheduleManager not ready when Node added {}", node.getHostId());
+            }
+        }
+    }
+
+    /**
+     * Handle a node switching state to DOWN.
+     */
+    public void onDown(final Node node)
+    {
+        LOG.debug("{} switched state to DOWN.", node);
+    }
+
+    /**
+     * Handle a new node being added to the cluster.
+     */
+    public void onAdd(final Node node)
+    {
+        if (myAgentNativeConnectionProvider == null || myAgentNativeConnectionProvider.confirmNodeValid(node))
+        {
+            LOG.info("Node added {}", node.getHostId());
+            NodeAddedAction callable = new NodeAddedAction(myEccNodesSync, myJmxConnectionProvider,
+                    myAgentNativeConnectionProvider, node);
+            myService.submit(callable);
+            if (myWorkerManager != null)
+            {
+                myWorkerManager.addNode(node);
+            }
+            if (myScheduleManager != null)
+            {
+                myScheduleManager.createScheduleFutureForNode(node.getHostId());
+            }
+            else
+            {
+                LOG.debug("myScheduleManager not ready when Node added {}", node.getHostId());
+            }
+        }
+    }
+
+    /**
+     * Handle a node being removed from the cluster.
+     */
+    public void onRemove(final Node node)
+    {
+        if (myAgentNativeConnectionProvider == null || myAgentNativeConnectionProvider.confirmNodeValid(node))
+        {
+            LOG.info("Node removed {}", node.getHostId());
+            NodeRemovedAction callable = new NodeRemovedAction(myEccNodesSync, myJmxConnectionProvider,
+                    myAgentNativeConnectionProvider, node);
+            myService.submit(callable);
+            if (myWorkerManager != null)
+            {
+                myWorkerManager.removeNode(node);
+            }
+            if (myScheduleManager != null)
+            {
+                myScheduleManager.removeScheduleFutureForNode(node.getHostId());
+            }
+        }
+    }
+}
---
+++ core.impl/src/test/java/com/ericsson/bss/cassandra/ecchronos/core/impl/repair/TestNodeLifecycleHandler.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2024 Telefonaktiebolaget LM Ericsson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.ericsson.bss.cassandra.ecchronos.core.impl.repair;
+
+import com.datastax.oss.driver.api.core.metadata.Node;
+import com.ericsson.bss.cassandra.ecchronos.connection.DistributedJmxConnectionProvider;
+import com.ericsson.bss.cassandra.ecchronos.connection.DistributedNativeConnectionProvider;
+import com.ericsson.bss.cassandra.ecchronos.core.impl.multithreads.NodeWorkerManager;
+import com.ericsson.bss.cassandra.ecchronos.core.repair.scheduler.ScheduleManager;
+import com.ericsson.bss.cassandra.ecchronos.data.sync.EccNodesSync;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestNodeLifecycleHandler
+{
+    @Mock
+    private EccNodesSync mockEccNodesSync;
+    @Mock
+    private DistributedJmxConnectionProvider mockJmxProvider;
+    @Mock
+    private DistributedNativeConnectionProvider mockNativeProvider;
+    @Mock
+    private NodeWorkerManager mockWorkerManager;
+    @Mock
+    private ScheduleManager mockScheduleManager;
+    @Mock
+    private ExecutorService mockExecutor;
+    @Mock
+    private Node mockNode;
+
+    private final UUID nodeId = UUID.randomUUID();
+    private NodeLifecycleHandler handler;
+
+    @Before
+    public void setup()
+    {
+        when(mockNode.getHostId()).thenReturn(nodeId);
+        handler = new NodeLifecycleHandler(mockEccNodesSync, mockJmxProvider,
+                mockNativeProvider, mockWorkerManager, mockScheduleManager, mockExecutor);
+    }
+
+    @Test
+    public void testOnAddValidNodeSubmitsTaskAndAddsToWorker()
+    {
+        when(mockNativeProvider.confirmNodeValid(mockNode)).thenReturn(true);
+
+        handler.onAdd(mockNode);
+
+        verify(mockExecutor).submit(any(Callable.class));
+        verify(mockWorkerManager).addNode(mockNode);
+        verify(mockScheduleManager).createScheduleFutureForNode(nodeId);
+    }
+
+    @Test
+    public void testOnAddInvalidNodeDoesNothing()
+    {
+        when(mockNativeProvider.confirmNodeValid(mockNode)).thenReturn(false);
+
+        handler.onAdd(mockNode);
+
+        verify(mockExecutor, never()).submit(any(Callable.class));
+        verify(mockWorkerManager, never()).addNode(any());
+    }
+
+    @Test
+    public void testOnRemoveValidNodeSubmitsTaskAndRemovesFromWorker()
+    {
+        when(mockNativeProvider.confirmNodeValid(mockNode)).thenReturn(true);
+
+        handler.onRemove(mockNode);
+
+        verify(mockExecutor).submit(any(Callable.class));
+        verify(mockWorkerManager).removeNode(mockNode);
+        verify(mockScheduleManager).removeScheduleFutureForNode(nodeId);
+    }
+
+    @Test
+    public void testOnRemoveInvalidNodeDoesNothing()
+    {
+        when(mockNativeProvider.confirmNodeValid(mockNode)).thenReturn(false);
+
+        handler.onRemove(mockNode);
+
+        verify(mockExecutor, never()).submit(any(Callable.class));
+        verify(mockWorkerManager, never()).removeNode(any());
+    }
+
+    @Test
+    public void testOnUpExistingNodeRefreshesConnection()
+    {
+        Map<UUID, Node> nodes = new HashMap<>();
+        nodes.put(nodeId, mockNode);
+        when(mockNativeProvider.getNodes()).thenReturn(nodes);
+
+        handler.onUp(mockNode);
+
+        verify(mockExecutor).submit(any(Callable.class));
+        verify(mockScheduleManager).createScheduleFutureForNode(nodeId);
+        verify(mockWorkerManager, never()).addNode(any());
+    }
+
+    @Test
+    public void testOnUpUnknownNodeDelegatesToOnAdd()
+    {
+        when(mockNativeProvider.getNodes()).thenReturn(new HashMap<>());
+        when(mockNativeProvider.confirmNodeValid(mockNode)).thenReturn(true);
+
+        handler.onUp(mockNode);
+
+        verify(mockWorkerManager).addNode(mockNode);
+        verify(mockScheduleManager).createScheduleFutureForNode(nodeId);
+    }
+}
